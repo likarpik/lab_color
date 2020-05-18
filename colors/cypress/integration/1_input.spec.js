114968611bc93aec78341724ab8a5a2933a19ec7
likarpik <karpik.lika@mail.ru>
2020-05-18 13:06:39 +0300

    Update 1_input.spec.js

--- colors/cypress/integration/1_input.spec.js
+++ colors/cypress/integration/1_input.spec.js
@@ -13,7 +13,7 @@ describe('Check the input form', () => {
      })
  
      it('Check input 2', () => {
-         const text = 'red'
+         const text = 'f234fa'
          cy.get('#choose_color').type(text).should('have.value', text)
          cy.get('button').click()
      })
