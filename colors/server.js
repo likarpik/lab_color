7948bb62e2be305b662fc7f30a55b6704f9e5a34
likarpik <karpik.lika@mail.ru>
2020-05-26 18:58:55 +0300

    Update server.js

--- colors/server.js
+++ colors/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '.', 'dist');
 const port = process.env.PORT || 3000;
+app.use(favicon(`${__dirname}/build/favicon.ico`));
 app.use(express.static(publicPath));
 app.get('*', (req, res) => {
    res.sendFile(path.join(publicPath, 'index.html'));
