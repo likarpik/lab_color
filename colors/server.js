242a40295c14cdd42cf6a9876cbbee8b85fb05b4
likarpik <karpik.lika@mail.ru>
2020-05-26 19:11:22 +0300

    Heroku test

--- colors/server.js
+++ colors/server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '.', 'dist');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 app.use(favicon(`${__dirname}/build/favicon.ico`));
 app.use(express.static(publicPath));
 app.get('*', (req, res) => {
